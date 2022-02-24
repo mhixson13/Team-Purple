dc2133e71ccd0a8cd682f49603f97c5271e6ad26
Julio Lopez <juliol2540@gmail.com>
2022-02-24 14:53:37 -0600

    Initial Maven Project

---
+++ evaluation-project/src/main/java/evaluation.java
@@ -0,0 +1,19 @@
+/* 
+    Team Purple
+    Julio Lopez, Marlon Miller, Miranda Hixson
+    Peer Evaluation Project
+    2/24/22
+    CS 375
+
+    javac evaluation.java
+    java evaluation
+*/
+
+public class evaluation {
+    public static void main(String[] args) {
+        System.out.println("\nEvaluation Project by Team Purple\n");
+    }
+
+    // Project Code Here
+
+}
---
+++ evaluation-project/src/test/java/evaluationTest.java
@@ -0,0 +1,38 @@
+/* 
+    Team Purple
+    Julio Lopez, Marlon Miller, Miranda Hixson
+    Peer Evaluation Project
+    2/24/22
+    CS 375
+
+    mvn compile
+    mvn clean
+    mvn test
+*/
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.lang.Math;
+import org.junit.*;
+
+public class evaluationTest {
+    evaluation eval;
+
+    @Before
+    public void initialize() {
+        eval = new evaluation();
+    }
+
+    @Test
+    public void sampleTest() {
+        try {
+            int a = 1, b = 1, c = 2;
+            assertEquals(c, a + b);
+        } catch(Exception e) {
+            System.out.println("\nTest Failed.\n");
+        }
+    }
+
+
+
+}

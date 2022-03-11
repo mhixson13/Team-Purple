6755e1037f21511b74cef51d9e3f5facc9b600bb
Julio Lopez <juliol2540@gmail.com>
2022-03-10 21:39:03 -0600

    Tests Added. Checkpoint 06 Ready

--- evaluation-project/src/test/java/evaluationTest.java
+++ evaluation-project/src/test/java/evaluationTest.java
@@ -86,4 +86,79 @@ public class evaluationTest {
             System.out.println("\nTest Failed");
         }
     }
+
+    @Test
+    public void test6() {
+        String s = "1, 000561003, 000123456, 8, 4";
+	    char actual = '4';
+        try {
+            char c = s.charAt(s.length()-1);
+            assertEquals(actual, c);
+        } catch (Exception e) {
+            System.out.println("\nTest 6 Failed.");
+        }
+    }
+
+    @Test 
+    public void test7() {
+        String s = "1, 000561003, 000123456, 8, 5";
+	    char actual = '5'; 
+        try {
+            char c = s.charAt(s.length()-1);
+            assertEquals(actual, c);
+        } catch (Exception e) {
+            System.out.println("\nTest 7 Failed."); 
+        }
+    }
+
+    @Test
+    public void test8() {
+        String s = "1, 000561003, 000123456, 8, 5";
+        int actual = 4, count = 0, i = 0;
+
+        try {
+            for(char c: s.toCharArray()) {
+                if(s.charAt(i) == ',') {
+                    count++;
+                }
+                i++;
+            }
+            assertEquals(actual, count);
+       } catch (Exception e) {
+            System.out.println("\nTest 8 Failed.");
+       }
+    }
+
+    @Test
+    public void test9() {
+        String s = "1, 000561003, 000123456, 8, 5";
+        int actual = 29, count = 0;
+
+        try {
+            for(char c: s.toCharArray()) {
+                count++;
+            }
+            assertEquals(actual, count);
+        } catch (Exception e) {
+            System.out.println("\nTest 9 Failed.");
+        }
+    }
+
+    @Test
+    public void test10() {
+        String s = "1, 000561003, 000123456, 8, 5"; 
+        int count = 0, i = 0, actual = 9;
+        try {
+            for(char c: s.toCharArray()) {
+                if(i >=3 && i <= 11) {
+                    count++;
+                }
+                i++;
+            }
+            assertEquals(actual, count);
+        } catch (Exception e) {
+            System.out.println("\nTest 10 Failed.");
+        }
+    }
+
 }

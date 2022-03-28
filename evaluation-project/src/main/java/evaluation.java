41c6b2a19c5732c9d118b67653aa36822de19445
Marlon_Miller <mem19b@acu.edu>
2022-03-28 13:28:37 -0500

    Database Connection

--- evaluation-project/src/main/java/evaluation.java
+++ evaluation-project/src/main/java/evaluation.java
@@ -5,8 +5,9 @@
     2/24/22
     CS 375
 
-    javac evaluation.java
-    java evaluation
+    Instructions:
+    javac -cp ".:postgresql-42.3.3.jar"  evaluation.java
+    java -cp postgresql-42.3.3.jar evaluation.java
 */
 
 import java.io.File;
@@ -23,125 +24,153 @@ import java.sql.SQLException;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.sql.ResultSet;
 //import java.postgresql-42.3.3;
 
-class Student {
-    public int studentID;
-    public String Period;
+// class Student {
+//     public int studentID;
+//     public String Period;
 
-    public Student(int stuID, String Class) {
-        this.studentID = stuID;
-        this.Period = Class;
-    }
-}
-class Teacher {
-    public int teacherID;
-}
-class Question {
-    public int QuestionID;
-    public String TheQuestion;
-    public String QuestionType;
+//     public Student(int stuID, String Class) {
+//         this.studentID = stuID;
+//         this.Period = Class;
+//     }
+// }
+// class Teacher {
+//     public int teacherID;
+// }
+// class Question {
+//     public int QuestionID;
+//     public String TheQuestion;
+//     public String QuestionType;
 
-    public Question(int QuestionID, String TheQuestion, String QuestionType) {
-        this.QuestionID = QuestionID;
-        this.TheQuestion = TheQuestion;
-        this.QuestionType = QuestionType;
-    }
+//     public Question(int QuestionID, String TheQuestion, String QuestionType) {
+//         this.QuestionID = QuestionID;
+//         this.TheQuestion = TheQuestion;
+//         this.QuestionType = QuestionType;
+//     }
 
-    public int getQuestionID() {
-        return this.QuestionID;
-    }
-    public String getTheQuestion() {
-        return this.TheQuestion;
-    }
-    public String getQuestionType() {
-        return this.QuestionType;
-    }
-}
-class User {
-    public String firstName;
-    public String lastName;
-    public int ID;
+//     public int getQuestionID() {
+//         return this.QuestionID;
+//     }
+//     public String getTheQuestion() {
+//         return this.TheQuestion;
+//     }
+//     public String getQuestionType() {
+//         return this.QuestionType;
+//     }
+// }
+// class User {
+//     public String firstName;
+//     public String lastName;
+//     public int ID;
 
-    public Question[] question = new Question[100];
+//     public Question[] question = new Question[100];
 
-    public User(String firstName, String lastName, int ID) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.ID = ID;
-    }
+//     public User(String firstName, String lastName, int ID) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.ID = ID;
+//     }
 
-    // public String getQuestion(int n) {
-    //     return this.question[n].TheQuestion;
-    // }
-    public void addRandomQs(int n) {
-        String[] array = {"T/F","MC","LK4","SA"};
-        int m = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(m == 4) 
-                m = 0;
-            this.question[i] = new Question(i,"Question? " + i,array[m++]);
-        }
-    }
+//     // public String getQuestion(int n) {
+//     //     return this.question[n].TheQuestion;
+//     // }
+//     public void addRandomQs(int n) {
+//         String[] array = {"T/F","MC","LK4","SA"};
+//         int m = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(m == 4) 
+//                 m = 0;
+//             this.question[i] = new Question(i,"Question? " + i,array[m++]);
+//         }
+//     }
 
-    public String getFirstName() {
-        return this.firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return this.lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+//     public String getFirstName() {
+//         return this.firstName;
+//     }
+//     public void setFirstName(String firstName) {
+//         this.firstName = firstName;
+//     }
+//     public String getLastName() {
+//         return this.lastName;
+//     }
+//     public void setLastName(String lastName) {
+//         this.lastName = lastName;
+//     }
     
-}
+// }
 
 // ---------------------------------
 
 public class evaluation {
 
     public static String csv_file = "ex.csv";
+
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
+        Connection c = null;
 
-        String jdbcURL = "jdbc:postgresql://localhost:5432/cs375v1";
-//        String username = "mrblee";
-//        String password = "purplewhite";
-        
-        System.out.println("\n\nWelcome to your Peer Evaluation Terminal Interface!");
-        System.out.println("Created by Team-Purple\n");
+        while(true) {
+            System.out.print("Username: ");
+            String username = sc.nextLine();
+            System.out.print("Password: ");
+            String password = sc.nextLine();
 
-        System.out.println(" *Documentation:                   https://github.com/mhixson13/Team-Purple/tree/main/Documentation");
-        System.out.println(" *UML Diagrams:                    https://github.com/mhixson13/Team-Purple/tree/main/uml");
-        System.out.println(" *More:                            https://github.com/mhixson13/Team-Purple\n");
-        
-        System.out.print("Username: ");
-        String username = sc.nextLine();
-        System.out.print("Password: ");
-        String password = sc.nextLine();
-        
-        try {
-            Connection connection = DriverManager.getConnection(jdbcURL,username,password);
-            System.out.println("Connected to PostgreSQL server");
-            
-            String sql = "insert into class (class_id, class_name) values "
-            		+ "(5005,'SE1');";
-            
-            Statement statement = connection.createStatement();
-            int rows = statement.executeUpdate(sql);
-            if(rows > 0) {
-            	System.out.println("A new statement is being printed");
+            try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs375v1",
+            username, password);
+            break;
+            } catch (Exception e) {
+                System.out.println("\nYour account or password is incorrect.\n");
+                // e.printStackTrace();
+                // System.err.println(e.getClass().getName()+": "+e.getMessage());
+                // System.exit(0);
+                continue;
             }
-            
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println("Error in connecting to PostgreSQL server");
-            e.printStackTrace();
         }
+        
+        System.out.println("Opened database successfully");
+        
+        
+
+//         String jdbcURL = "jdbc:postgresql://localhost:5432/cs375v1";
+// //        String username = "mrblee";
+// //        String password = "purplewhite";
+        
+//         System.out.println("\n\nWelcome to your Peer Evaluation Terminal Interface!");
+//         System.out.println("Created by Team-Purple\n");
+
+//         System.out.println(" *Documentation:                   https://github.com/mhixson13/Team-Purple/tree/main/Documentation");
+//         System.out.println(" *UML Diagrams:                    https://github.com/mhixson13/Team-Purple/tree/main/uml");
+//         System.out.println(" *More:                            https://github.com/mhixson13/Team-Purple\n");
+        
+//         System.out.print("Username: ");
+//         String username = sc.nextLine();
+//         System.out.print("Password: ");
+//         String password = sc.nextLine();
+        
+//         try {
+//             Connection connection = DriverManager.getConnection(jdbcURL,username,password);
+//             System.out.println("Connected to PostgreSQL server");
+            
+//             String sql = "insert into class (class_id, class_name) values "
+//             		+ "(5005,'SE1');";
+            
+//             Statement statement = connection.createStatement();
+//             int rows = statement.executeUpdate(sql);
+//             if(rows > 0) {
+//             	System.out.println("A new statement has been inserted");
+//             }
+            	
+//             connection.close();
+//         } catch (SQLException e) {
+//             System.out.println("Error in connecting to PostgreSQL server");
+//             e.printStackTrace();
+//         }
 
 
         // System.out.println("\n\nWelcome to your Peer Evaluation Terminal Interface!");
@@ -196,6 +225,7 @@ public class evaluation {
             e.printStackTrace();
         }
     }
+}
 
     // Parse CSV with OpenCSV Library
     // public static void parseCSV() {
@@ -219,5 +249,20 @@ public class evaluation {
     //     } catch (Exception e) {
     //         e.printStackTrace();        }
     // }
-    
-}
+
+// -----------------------------
+
+//            System.out.println("\nThis are the following Evaluations");
+//            sql = "select class_name from class;";
+//            
+//            ResultSet result = statement.executeQuery(sql);
+//            System.out.print(result);
+//            
+//            while(result.next()) {
+//            	String classes = result.getString("class_name");
+////            	String evalid = result.getString("evalid");
+////            	String questionid = result.getString("question_id");
+////            	String text = result.getString("text");
+//            	
+//            	System.out.printf("%d - %s - %s - %s\n", classes);
+//            }
